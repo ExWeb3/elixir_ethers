d53c472b0f93cdd4b980ed5cc7cd24cbe267c3e5
Alisina Bahadori <alisina.bm@gmail.com>
2023-09-02 23:44:04 -0400

    Implement BlockUtils
    Add tests for block utils

--- test/support/test_rpc_module.ex
+++ test/support/test_rpc_module.ex
@@ -1,6 +1,8 @@
 defmodule Ethers.TestRPCModule do
   @moduledoc false
 
+  import Ethers.Utils
+
   def eth_estimate_gas(_params, _opts) do
     {:ok, "0x100"}
   end
@@ -15,5 +17,19 @@ defmodule Ethers.TestRPCModule do
     end
 
     Ethereumex.HttpClient.eth_call(params, block, opts)
+  end
+
+  def eth_block_number(opts) do
+    {:ok, opts[:block]}
+  end
+
+  def eth_get_block_by_number(number, _include_all?, opts) do
+    timestamp =
+      number
+      |> hex_to_integer!()
+      |> Kernel.+(opts[:timestamp])
+      |> integer_to_hex()
+
+    {:ok, %{"timestamp" => timestamp}}
   end
 end

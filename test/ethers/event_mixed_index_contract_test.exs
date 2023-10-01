6c665126faf119d772c37c1d977d4ecc0d78bde2
Alisina Bahadori <alisina.bm@gmail.com>
2023-09-30 20:17:15 -0400

    Add more tests for get_log with mixed event index

--- test/ethers/event_mixed_index_contract_test.exs
+++ test/ethers/event_mixed_index_contract_test.exs
@@ -8,7 +8,7 @@ defmodule Ethers.EventMixedIndexContractTest do
 
   alias Ethers.Contract.Test.EventMixedIndexContract
 
-  @test_address "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
+  @from "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
 
   describe "event filters" do
     test "works with mixed indexed events" do
@@ -28,7 +28,36 @@ defmodule Ethers.EventMixedIndexContractTest do
                  "0x00000000000000000000000090f8bf6a479f320ead074411a4b0e7944ea8c9c1",
                  "0x00000000000000000000000090f8bf6a479f320ead074411a4b0e7944ea8c9c1"
                ]
-             } == EventMixedIndexContract.EventFilters.transfer(@test_address, @test_address)
+             } == EventMixedIndexContract.EventFilters.transfer(@from, @from)
+    end
+
+    test "can filter and show the correct events" do
+      init_params = EventMixedIndexContract.constructor()
+      assert {:ok, tx_hash} = Ethers.deploy(EventMixedIndexContract, init_params, %{from: @from})
+      assert {:ok, address} = Ethers.deployed_address(tx_hash)
+
+      EventMixedIndexContract.transfer(100, @from, true, @from)
+      |> Ethers.send!(to: address, from: @from)
+
+      filter = EventMixedIndexContract.EventFilters.transfer(@from, @from)
+
+      assert [
+               %Ethers.Event{
+                 address: ^address,
+                 topics: ["Transfer(uint256,address,bool,address)", @from, @from],
+                 data: [100, true],
+                 removed: false,
+                 log_index: 0,
+                 transaction_index: 0,
+                 topics_raw: [
+                   "0x0f1459b71050cedb12633644ebaa16569e1bb49626ab8a0f4c7d1cf0d574abe7",
+                   "0x00000000000000000000000090f8bf6a479f320ead074411a4b0e7944ea8c9c1",
+                   "0x00000000000000000000000090f8bf6a479f320ead074411a4b0e7944ea8c9c1"
+                 ],
+                 data_raw:
+                   "0x00000000000000000000000000000000000000000000000000000000000000640000000000000000000000000000000000000000000000000000000000000001"
+               }
+             ] = Ethers.get_logs!(filter)
     end
   end
 end
--- test/support/contracts/event_mixed_index.sol
+++ test/support/contracts/event_mixed_index.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.8.17;
 
 contract EventMixedIndex {
+    function transfer(
+        uint256 amount,
+        address sender,
+        bool isFinal,
+        address receiver
+    ) public {
+        emit Transfer(amount, sender, isFinal, receiver);
+    }
+
     event Transfer(
         uint256 amount,
         address indexed sender,

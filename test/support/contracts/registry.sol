11472e868465860c8c1c472e22f69e05593cb419
Blaze <blaze@dorime.org>
2023-10-23 11:07:01 +0100

    Revert "test: add registry as tuple and array"
    This reverts commit ee82544e2de58f8419670bba3e4f1a5272ca941b.

--- test/support/contracts/registry.sol
+++ test/support/contracts/registry.sol
@@ -18,22 +18,5 @@ contract Registry {
         return registry[owner];
     }
 
-    function infoMany(
-        address[] calldata owners
-    ) public view returns (RegisterParams[] memory) {
-        RegisterParams[] memory params = new RegisterParams[](owners.length);
-
-        for (uint256 i = 0; i < owners.length; i++)
-            params[i] = registry[owners[i]];
-
-        return params;
-    }
-
-    function infoAsTuple(
-        address owner
-    ) public view returns (string memory, uint8) {
-        return (registry[owner].name, registry[owner].age);
-    }
-
     event Registered(address indexed, RegisterParams);
 }

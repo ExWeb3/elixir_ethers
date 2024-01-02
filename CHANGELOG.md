465a8392d1d7baa21deeaac2b08828450fc56d6a
Alisina Bahadori <alisina.bm@gmail.com>
2024-01-01 20:46:11 -0500

    Bump version to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## v0.2.0 (2024-01-01)
 
 ### New Features
 
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Ethers.MixProject do
   use Mix.Project
 
-  @version "0.2.0-dev"
+  @version "0.2.0"
   @source_url "https://github.com/alisinabh/elixir_ethers"
 
   def project do

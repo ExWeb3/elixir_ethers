64cb7f6b82be2b62c3da6e4816fc25a0f4a96ca9
joaop21 <jpsilva9898@gmail.com>
2024-11-18 16:27:09 +0000

    add ethereumex http client adapter implementation

---
+++ lib/ethers/rpc_client/ethereumex_http_client.ex
@@ -0,0 +1,38 @@
+defmodule Ethers.RpcClient.EthereumexHttpClient do
+  @behaviour Ethers.RpcClient.Adapter
+
+  @exclude_delegation [:eth_get_logs]
+
+  for {func, arity} <- Ethers.RpcClient.Adapter.behaviour_info(:callbacks),
+      func not in @exclude_delegation do
+    args = Macro.generate_arguments(arity, __MODULE__)
+    @impl true
+    def unquote(func)(unquote_splicing(args)) do
+      apply(Ethereumex.HttpClient, unquote(func), [unquote_splicing(args)])
+    end
+  end
+
+  @impl true
+  def eth_get_logs(params, opts) do
+    params =
+      params
+      |> replace_key(:from_block, :fromBlock)
+      |> replace_key(:to_block, :toBlock)
+
+    IO.inspect(params)
+
+    Ethereumex.HttpClient.eth_get_logs(params, opts)
+  end
+
+  defp replace_key(map, old_key, new_key) do
+    case Map.fetch(map, old_key) do
+      {:ok, value} ->
+        map
+        |> Map.put(new_key, value)
+        |> Map.delete(old_key)
+
+      :error ->
+        map
+    end
+  end
+end

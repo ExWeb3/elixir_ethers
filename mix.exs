60915f1ed1f8acf668ad3b749dfa8ea017c19e29
Alisina Bahadori <alisina.bm@gmail.com>
2025-01-10 11:38:44 -0500

    Update ethereumex version requirement

--- mix.exs
+++ mix.exs
@@ -104,7 +104,7 @@ defmodule Ethers.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
-      {:ethereumex, "~> 0.10", github: "alisinabh/ethereumex", branch: "add-blob-base-fee"},
+      {:ethereumex, "~> 0.10 and >= 0.10.7"},
       {:ex_abi, "~> 0.8.0", optional: System.get_env("SKIP_EX_KECCAK") == "true"},
       {:ex_doc, "~> 0.32", only: :dev, runtime: false},
       {:ex_keccak, "~> 0.7.5"},

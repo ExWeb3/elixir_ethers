71938c8aae6269c579f0580f5f2bb66e24d9f0fc
Alisina Bahadori <alisina.bm@gmail.com>
2025-10-24 08:01:13 -0400

    Upgrade CI elixir/erlang and bump minimum to 1.15 (#230)

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Ethers.MixProject do
     [
       app: :ethers,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       name: "Ethers",
